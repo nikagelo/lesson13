4f20978c33561313ecfbdee05248cabf7ef12f3d
nikagelo <nikagelo@gmail.com>
2022-06-21 17:56:09 +0400

    final version

---
+++ js/app.js
@@ -0,0 +1,27 @@
+let myString = "I love coding";
+let myNum = 20;
+let booleanTrue = true;
+let booleanFalse = false;
+let myUndefined = undefined;
+let myNull = null;
+
+const myName = "Nika";
+const myHobby = "Coding";
+const yearOfBirth = 2000;
+let currentYear = 2022;
+
+let age = currentYear - yearOfBirth;
+
+let text1 =
+  "My name is " +
+  myName +
+  ", I am " +
+  age +
+  " old and my hobby is " +
+  myHobby +
+  ".";
+console.log(text1);
+
+let text2 = `My name is ${myName}, I am ${age} old and my hobby is ${myHobby}`;
+
+console.log(text2);
